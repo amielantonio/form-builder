c8a81532e6ca8b35ca2cfe45cd181fb33db1f685
Amiel Antonio <amielnicole.antonio@gmail.com>
2018-12-20 16:57:18 +0800

    made it into a jquery plugin

--- release/js/app.js
+++ release/js/app.js
@@ -86,14 +86,36 @@
 /************************************************************************/
 /******/ ({
 
-/***/ "./lib/js/components/create_elements.js":
-/*!**********************************************!*\
-  !*** ./lib/js/components/create_elements.js ***!
-  \**********************************************/
+/***/ "./lib/js/canvas/canvas_draggable.js":
+/*!*******************************************!*\
+  !*** ./lib/js/canvas/canvas_draggable.js ***!
+  \*******************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-eval("\n\n//# sourceURL=webpack:///./lib/js/components/create_elements.js?");
+eval("$( \".form-builder-canvas .form-builder-tool\" ).draggable({\r\n  revert: 'invalid',\r\n  opacity: 0.8,\r\n  helper: 'clone',\r\n  cursor: '',\r\n  cursorAt: {},\r\n  start: function(){\r\n\r\n  },\r\n  drag: function(){\r\n\r\n  },\r\n  stop: function(){\r\n\r\n  }\r\n});\r\n\n\n//# sourceURL=webpack:///./lib/js/canvas/canvas_draggable.js?");
+
+/***/ }),
+
+/***/ "./lib/js/canvas/canvas_droppable.js":
+/*!*******************************************!*\
+  !*** ./lib/js/canvas/canvas_droppable.js ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("$( '' ).droppable({\r\n  accept: '',\r\n  greedy: '',\r\n  drop: function(){\r\n\r\n  }\r\n});\r\n\n\n//# sourceURL=webpack:///./lib/js/canvas/canvas_droppable.js?");
+
+/***/ }),
+
+/***/ "./lib/js/canvas/canvas_sortable.js":
+/*!******************************************!*\
+  !*** ./lib/js/canvas/canvas_sortable.js ***!
+  \******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("$( '.form-builder-canvas .form-builder-sotable' ).sortable({\r\n  placeholder: \"item-highlight-sorting\",\r\n  opacity: 0.8,\r\n  start: function(){\r\n\r\n  }\r\n});\r\n\n\n//# sourceURL=webpack:///./lib/js/canvas/canvas_sortable.js?");
 
 /***/ }),
 
@@ -104,7 +126,7 @@ eval("\n\n//# sourceURL=webpack:///./lib/js/components/create_elements.js?");
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-eval("/**\r\n *\r\n */\r\n\r\n__webpack_require__( /*! ./components/create_elements */ \"./lib/js/components/create_elements.js\" );\r\n\n\n//# sourceURL=webpack:///./lib/js/entry.js?");
+eval("( function ($){\r\n\r\n  $.fn.formBuilder = function(){\r\n\r\n    return $(this).append( createBuilder() );\r\n\r\n  };\r\n\r\n  function createBuilder(){\r\n\r\n    var element = createContainer();\r\n    var canvas = createCanvas();\r\n    var sortable = createSortable();\r\n    var sidebar = createSidebar();\r\n\r\n    canvas.append( sortable );\r\n\r\n    element.append( sidebar );\r\n    element.append( canvas );\r\n\r\n    return element;\r\n\r\n    function createContainer(){\r\n      return $( '<div>',{\r\n        class: \"form-builder-container\"\r\n      });\r\n    }\r\n\r\n    function createCanvas(){\r\n      return $( '<div>',{\r\n        class: \"form-builder-canvas\"\r\n      });\r\n    }\r\n\r\n    function createSortable(){\r\n      return $( '<div>' , {\r\n        class: \"form-builder-sortable\"\r\n      });\r\n    }\r\n\r\n    function createSidebar(){\r\n      var sidebar = $( \"<div>\", {\r\n        class: \"form-builder-sidebar\"\r\n      });\r\n\r\n      var widgets = {\r\n        \"row\": {\r\n          \"html\": \"div\",\r\n          \"name\": \"Row\",\r\n          \"options\": \"row\",\r\n          'icon': \"fas fa-th-large\"\r\n        },\r\n        \"textbox\": {\r\n          \"html\": \"input\",\r\n          \"name\": \"Textbox\",\r\n          \"options\": \"text\",\r\n          'icon': \"fas fa-font\"\r\n        },\r\n        \"email\": {\r\n          \"html\": \"input\",\r\n          \"name\": \"Email\",\r\n          \"options\": \"text\",\r\n          'icon': \"far fa-envelope\"\r\n        },\r\n        \"radio\": {\r\n          \"html\": \"input\",\r\n          \"name\": \"Radio\",\r\n          \"options\": \"choices\",\r\n          'icon': \"far fa-check-circle\"\r\n        },\r\n        \"checkbox\": {\r\n          \"html\": \"input\",\r\n          \"name\": \"Checkbox\",\r\n          \"options\": \"choices\",\r\n          'icon': \"far fa-check-square\"\r\n        },\r\n        \"date\": {\r\n          \"html\": \"input\",\r\n          \"name\": \"Date\",\r\n          \"options\": \"date\",\r\n          'icon': \"far fa-calendar-alt\"\r\n        },\r\n        \"password\": {\r\n          \"html\": \"input\",\r\n          \"name\": \"Password\",\r\n          \"options\": \"text\",\r\n          'icon': \"fas fa-lock\"\r\n        },\r\n        \"hidden\": {\r\n          \"html\": \"input\",\r\n          \"name\": \"Hidden\",\r\n          \"options\": \"hidden\",\r\n          'icon': \"fas fa-font\"\r\n        },\r\n        \"file\": {\r\n          \"html\": \"input\",\r\n          \"name\": \"file\",\r\n          \"options\": \"file\",\r\n          'icon': \"fas fa-file-upload\"\r\n        },\r\n        \"num\": {\r\n          \"html\": \"input\",\r\n          \"name\": \"Number\",\r\n          \"options\": \"text\",\r\n          'icon': \"fas fa-sort-numeric-up\"\r\n        },\r\n        \"tel\": {\r\n          \"html\": \"input\",\r\n          \"name\": \"Telephone\",\r\n          \"options\": \"text\",\r\n          'icon': \"fas fa-phone\"\r\n        },\r\n        \"select\": {\r\n          \"html\": \"select\",\r\n          \"name\": \"Dropdown\",\r\n          \"options\": \"dropdown\",\r\n          'icon': \"far fa-caret-square-down\"\r\n        },\r\n        \"textarea\": {\r\n          \"html\": \"textarea\",\r\n          \"name\": \"Textbox\",\r\n          \"options\": \"text\",\r\n          'icon': \"far fa-file-alt\"\r\n        },\r\n        \"button\": {\r\n          \"html\": \"button\",\r\n          \"name\": \"Button\",\r\n          \"options\": \"button\",\r\n          'icon': \"far fa-hand-point-up\"\r\n        },\r\n        \"paragraph\": {\r\n          \"html\": \"p\",\r\n          \"name\": \"Textbox\",\r\n          \"options\": \"paragraph\",\r\n          'icon': \"fas fa-paragraph\"\r\n        }\r\n      };\r\n\r\n      Object.keys( widgets ).forEach(function(k){\r\n\r\n        var icon = $( '<i>', {\r\n          class: widgets[k]['icon'] + \" fa-lg\"\r\n        });\r\n        var element = $( '<a>',{\r\n          class: \"form-builder-tool\",\r\n          \"data-build-type\": k,\r\n          \"data-html\": widgets[k]['html'],\r\n          \"data-build-options\": widgets[k]['options']\r\n        }).html( widgets[k]['name'] );\r\n\r\n        element.prepend( icon );\r\n        sidebar.append( element );\r\n\r\n      });\r\n\r\n      return sidebar;\r\n\r\n    }\r\n  }\r\n\r\n\r\n  function prepareCanvas() {\r\n    __webpack_require__( /*! ./canvas/canvas_draggable */ \"./lib/js/canvas/canvas_draggable.js\" );\r\n    __webpack_require__( /*! ./canvas/canvas_droppable */ \"./lib/js/canvas/canvas_droppable.js\" );\r\n    __webpack_require__( /*! ./canvas/canvas_sortable */ \"./lib/js/canvas/canvas_sortable.js\" );\r\n  }\r\n\r\n  function prepareTools(){\r\n\r\n  }\r\n\r\n  function prepareWidgets(){\r\n\r\n  }\r\n\r\n\r\n\r\n})(jQuery);\r\n\n\n//# sourceURL=webpack:///./lib/js/entry.js?");
 
 /***/ }),
 
